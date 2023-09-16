0e0eabcc669962af19efb40e71f79b3eab203a3c
mirshad517 <72601071+mirshad517@users.noreply.github.com>
2023-09-16 22:01:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 
 app = FastAPI(title="My Apis",
              description="Developer : Mirshad",
-             docExpansion="None")
+             docs_url=None,
+             redoc_url=None)
 
 #domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
